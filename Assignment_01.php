a919eb5ea261ddd317adf1e5c2cc50364f022f13
Mahmoud Abdullah <90221936+MahmoudAqela@users.noreply.github.com>
2023-09-06 17:47:33 +0300

    Add files via upload
    The first assignment.

---
+++ Assignment_01.php
@@ -0,0 +1,58 @@
+<!-- 
+    -- About Assignment:
+    -- -- Name: Assginment 01
+    -- -- Content: https://www.youtube.com/playlist?list=PLDoPjvoNmBAy41u35AqJUrI-H83DObUDq [1 : 5]
+    -- -- Presenter: Osama Elzero ( Elzero Web School - Youtube )
+    -- -- Date: 09-01-2023 ( September )
+    -- About Author:
+    -- -- Author: Mahmoud Abdullah Aqela
+    -- -- Education: FCI ( Faculty of Computers and Information - Minia University )
+    -- -- Age: 22
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+  </head>
+  <body>
+    <!-- Task1 - Multiple line comment  -->
+    <?php
+        /**
+         * ==============================================
+         * == This is Example of Multiple line comment ==
+         * ==============================================
+         */
+    ?>
+    <!-- Task2 - Different type of comments for every line  -->
+    <?php 
+        // echo 'Prevent Me From Running Please';
+        # echo 'Prevent Me From Running Please';
+        /* echo 'Prevent Me From Running Please'; */
+    ?>
+    <!-- Task3 - Correct comment syntacs  -->
+    <? 
+        /*
+         My Application
+         Version 1.0
+         Created By Elzero
+         */
+     ?>
+    <!-- Task4 - Choose the correct comment  -->
+    <?php 
+        // Question:
+            // ## First Comment
+            # // # Second Comment
+            /* /* /* Third Comment */
+            ////// Fourth Comment
+        // Answer:
+            // Lines 1, 2, 3 are Comments
+            // Line 4 is not the correct syntacs of comments
+    ?>
+    <div></div>
+    <div></div>
+  </body>
+</html>
